15a9cfe971d3ea2fa85c1916fb1b3adecb94f91c
Xuan_the_Coder <wx209dewang@gmail.com>
2020-10-31 17:41:47 -0500

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuestionComponent } from './question.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {FormsModule} from '@angular/forms';
+import { ApiService } from './api.service'
+
+@NgModule({
+  declarations: [
+    AppComponent, QuestionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    FormsModule
+  ],
+  providers: [ApiService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

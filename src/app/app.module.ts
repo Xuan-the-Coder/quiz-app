eae4be52542ac6c4cbb48e9ad67dece9c7742276
Xuan-the-Coder <wx209dewang@gmail.com>
2020-11-11 15:18:27 -0600

    incorporated login component into the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,8 @@ import { from, fromEventPattern } from 'rxjs';
 import { NavComponent } from './nav.component'
 import { QuizComponent } from './quiz.component'
 import { QuizzesComponent} from './quizzes.component'
-import { RegisterComponent} from './register.component'
+import { RegisterComponent } from './register.component'
+import { LoginComponent } from './login.component'
 import { AuthService } from './auth.service'
 import { AuthInterceptor} from './auth.interceptor'
 
@@ -29,6 +30,7 @@ const routes = [
   { path: 'question', component: QuestionComponent },
   { path: 'question/:quizId', component: QuestionComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'quiz', component: QuizComponent }
 ]
 @NgModule({
@@ -40,7 +42,8 @@ const routes = [
     NavComponent, 
     QuizComponent,
     QuizzesComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,

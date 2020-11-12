a785c9bb261b0953f0d1636fcbd06ecbdc0330e3
Xuan_the_Coder <wx209dewang@gmail.com>
2020-11-12 17:28:40 -0600

    created finished component to display a dialog box with scores user acheived after finishing the quiz

--- src/app/finished.component.ts
+++ src/app/finished.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   template: `
   <h1>Your Score</h1>
-  <h2>12/40</h2>
+  <h2>{{data.correct}}/{{data.total}}</h2>
   `
 })
 export class FinishedComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) private data){   
+  }
 }
+
